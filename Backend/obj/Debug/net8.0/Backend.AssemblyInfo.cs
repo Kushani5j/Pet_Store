a7884ae49eba0059cc6bc30e88f6087a5beab5b4
Kushani5j <kushani5j@gamil.com>
2025-03-20 17:17:47 +1030

    Added all files to the project

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c135e9bdf54d330349499eb00895053890398198")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25d92cf1c2c76d2e30e7c1e785f44785f7a0284b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
